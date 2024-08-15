eccf3d913e0b7fa441304561259740e4a65eb0d6
Shafi-Anwar <sufiyananwar559@gmail.com>
2024-08-15 17:22:13 +0530

    dess

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,17 +1,20 @@
-import { MongoClient } from "mongodb";
+// lib/mongodb.js
+import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable to store the client
-  if (!global._mongoClientPromise) {
-    global._mongoClientPromise = client.connect();
+  // In development mode, use a global variable to cache the client
+  if (global._mongoClientPromise) {
+    clientPromise = global._mongoClientPromise;
+  } else {
+    clientPromise = client.connect();
+    global._mongoClientPromise = clientPromise;
   }
-  clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, use a local variable
+  // In production mode, create a new client every time
   clientPromise = client.connect();
 }
 

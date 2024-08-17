f18c8d09a33a2e677f9abd7573cbd0f13355dbbc
Shafi-Anwar <sufiyananwar559@gmail.com>
2024-08-17 18:21:12 +0530

    developeradas

--- src/middleware.js
+++ src/middleware.js
@@ -1,25 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from 'next/server';
 
 export default function middleware(req) {
-  // Check if the environment supports Clerk middleware
-  const isEdge = process.env.NEXT_PUBLIC_RUNTIME === 'edge';
-  const isProduction = process.env.NODE_ENV === 'production';
-
-  if (isProduction && isEdge) {
-    // Return NextResponse if running in Edge runtime
-    return NextResponse.next();
-  }
-
-  // Use Clerk middleware if not in an Edge environment or not in production
-  return clerkMiddleware()(req);
+  // Logic for Edge-compatible operations
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };

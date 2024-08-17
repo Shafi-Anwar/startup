fa21eb477d825116d221c207aa197c338bcd2efc
Shafi-Anwar <sufiyananwar559@gmail.com>
2024-08-17 18:19:28 +0530

    developerad

--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,17 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from 'next/server';
 
 export default function middleware(req) {
-  // Check if the runtime environment supports Clerk or not
-  if (process.env.NODE_ENV !== 'production' || process.env.NEXT_PUBLIC_RUNTIME !== 'edge') {
-    return clerkMiddleware()(req);
+  // Check if the environment supports Clerk middleware
+  const isEdge = process.env.NEXT_PUBLIC_RUNTIME === 'edge';
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  if (isProduction && isEdge) {
+    // Return NextResponse if running in Edge runtime
+    return NextResponse.next();
   }
 
-  // For environments that don't support Clerk
-  return NextResponse.next();
+  // Use Clerk middleware if not in an Edge environment or not in production
+  return clerkMiddleware()(req);
 }
 
 export const config = {
